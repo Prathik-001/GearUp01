9e97197127c2c3363ef9aabf2bff96a5f7552760
Prathik <prathikhegdecoc@gmail.com>
2025-03-30 11:21:05 +0530

    env

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -5,6 +5,7 @@ const conf = {
     appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBikeCollectionId:String(import.meta.env.VITE_APPWRITE_BIKE_COLLECTION_ID),
     appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteUserCollectionId:String(import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID),
 }
 
 export default conf
